f8cd965baef9a10221d4467329586e02a7e0e0dd
Factorial <27factorial@gmail.com>
2020-04-04 08:10:13 -0700

    Add Signed trait and add methods to Real. Change into_* to to_real().

--- src/traits.rs
+++ src/traits.rs
@@ -1,7 +1,8 @@
-/// A value which has some sensible notion of arithmetic,
-/// a [`Debug`][1] implementation, and is [`Copy`]. This
-/// trait is sealed, and can not be implemented outside of
-/// `const_linear`.
+/// A trait implmented for types which have some sensible notion
+/// of arithmetic, a [`Debug`][1] implementation, and are [`Copy`].
+/// This trait is sealed, and can not be implemented outside of
+/// `const_linear`. It is implemented for every primitive
+/// numeric type.
 ///
 /// # Examples
 ///
@@ -20,8 +21,8 @@
 /// ```
 /// use const_linear::traits::Scalar;
 ///
-/// assert_eq!(1.to_f64(), 1 as f64);
-/// assert_eq!(1.to_f32(), 1 as f32);
+/// assert_eq!(1.to_real::<f64>(), 1 as f64);
+/// assert_eq!(1.to_real::<f32>(), 1 as f32);
 /// ```
 ///
 /// [1]: std::fmt::Debug
@@ -34,18 +35,59 @@ pub trait Scalar: sealed::Ops<Self> {
     /// multiplicative identity.
     const ONE: Self;
 
-    /// Converts `T` to an [`f64`]. Equivalent to `T as f64`.
-    fn to_f64(self) -> f64;
-
-    /// Converts `T` to an [`f32`]. Equivalent to `T as f32`.
-    fn to_f32(self) -> f32;
+    fn into_real<R: Real>(self) -> R;
 }
 
-/// A value which is a scalar value and is also
-/// capable of representing real numbers. This trait
-/// is sealed, and can not be implemented outside of
-/// `const_linear`.
-pub trait Real: Scalar + sealed::Ops<Self> {
+/// A trait
+pub trait Signed: Scalar + sealed::Ops<Self> {
+    fn negate(self) -> Self;
+
+    /// Returns true if self is positive and false if the
+    /// number is zero or negative.
+    fn is_positive(self) -> bool;
+
+    /// Returns true if self is negative and false if the
+    /// number is zero or positive.
+    fn is_negative(self) -> bool;
+}
+
+/// A trait that is implemented for types that can represent
+/// real numbers. This trait is sealed, and can not be
+/// implemented outside of `const_linear`. It is implemented
+/// for [`f64`] and [`f32`].
+pub trait Real: Signed + sealed::Ops<Self> {
+    fn floor(self) -> Self;
+    fn ceil(self) -> Self;
+    fn round(self) -> Self;
+    fn trunc(self) -> Self;
+    fn fract(self) -> Self;
+    fn powi(self, n: i32) -> Self;
+    fn powf(self, n: Self) -> Self;
+    fn sqrt(self) -> Self;
+    fn exp(self) -> Self;
+    fn exp2(self) -> Self;
+    fn ln(self) -> Self;
+    fn log(self, base: Self) -> Self;
+    fn log2(self) -> Self;
+    fn log10(self) -> Self;
+    fn cbrt(self) -> Self;
+    fn sin(self) -> Self;
+    fn cos(self) -> Self;
+    fn tan(self) -> Self;
+    fn asin(self) -> Self;
+    fn acos(self) -> Self;
+    fn atan(self) -> Self;
+    fn atan2(self, other: Self) -> Self;
+    fn sinh(self) -> Self;
+    fn cosh(self) -> Self;
+    fn tanh(self) -> Self;
+    fn asinh(self) -> Self;
+    fn acosh(self) -> Self;
+    fn atanh(self) -> Self;
+    fn recip(self) -> Self;
+    fn exp_m1(self) -> Self;
+    fn ln_1p(self) -> Self;
+
     /// Euler's number, [`e`](https://en.wikipedia.org/wiki/E_(mathematical_constant))
     fn e() -> Self;
 
@@ -114,53 +156,257 @@ pub trait Real: Scalar + sealed::Ops<Self> {
 }
 
 macro_rules! scalar_impls {
-    ($($t:ty, $zero:literal, $one:literal),+) => {
+    ($($t:ident, $zero:literal, $one:literal),+) => {
         $(
             impl $crate::traits::Scalar for $t {
                 const ZERO: Self = $zero;
                 const ONE: Self = $one;
 
-                fn to_f64(self) -> f64 {
-                    self as f64
-                }
-
-                fn to_f32(self) -> f32 {
-                    self as f32
-                }
-            }
-
-            // Unfortunately, it's not possible to do a blanket impl, so this is a workaround
-            // for all types which impl Scalar. Inverse of impl Mul<T> for Matrix<T>
-            impl<const M: usize, const N: usize> ::std::ops::Mul<$crate::Matrix<$t, { M }, { N }>> for $t {
-                type Output = $crate::core::Matrix<$t, { M }, { N }>;
-
-                fn mul(self, mut rhs: Self::Output) -> Self::Output {
-                    for elem in rhs.column_iter_mut() {
-                        *elem *= self;
-                    }
-
-                    rhs
+                fn into_real<R: Real>(self) -> R {
+                    R::from_scalar(self)
                 }
             }
         )*
     }
 }
 
-macro_rules! real_impls {
-    ($($t:ident),+) => {
+macro_rules! signed_impls {
+    ($($t:ty),+) => {
         $(
-            impl ::std::ops::Mul<$crate::geometry::angle::Angle<$t>> for $t {
-                type Output = $crate::geometry::angle::Angle<$t>;
+            impl $crate::traits::Signed for $t {
+                fn negate(self) -> Self {
+                    -self
+                }
 
-                fn mul(
-                    self,
-                    rhs: $crate::geometry::angle::Angle<$t>
-                ) -> $crate::geometry::angle::Angle<$t> {
-                    match rhs {
-                        $crate::geometry::angle::Angle::Deg(_) =>
-                            $crate::geometry::angle::Angle::Deg(rhs.val() * self),
-                        $crate::geometry::angle::Angle::Rad(_) =>
-                            $crate::geometry::angle::Angle::Rad(rhs.val() * self),
+                fn is_positive(self) -> bool {
+                    self > Self::ZERO
+                }
+
+                fn is_negative(self) -> bool {
+                    self < Self::ZERO
+                }
+            }
+        )*
+    };
+}
+
+macro_rules! real_impls {
+    ($($t:tt, $func:tt),+) => {
+        $(
+            impl Real for $t {
+                fn floor(self) -> Self {
+                    <$t>::floor(self)
+                }
+
+                fn ceil(self) -> Self {
+                    <$t>::ceil(self)
+                }
+
+                fn round(self) -> Self {
+                    <$t>::round(self)
+                }
+
+                fn trunc(self) -> Self {
+                    <$t>::trunc(self)
+                }
+
+                fn fract(self) -> Self {
+                    <$t>::fract(self)
+                }
+
+                fn powi(self, n: i32) -> Self {
+                    <$t>::powi(self, n)
+                }
+
+                fn powf(self, n: Self) -> Self {
+                    <$t>::powf(self, n)
+                }
+
+                fn sqrt(self) -> Self {
+                    <$t>::sqrt(self)
+                }
+
+                fn exp(self) -> Self {
+                    <$t>::exp(self)
+                }
+
+                fn exp2(self) -> Self {
+                    <$t>::exp2(self)
+                }
+
+                fn ln(self) -> Self {
+                    <$t>::ln(self)
+                }
+
+                fn log(self, base: Self) -> Self {
+                    <$t>::log(self, base)
+                }
+
+                fn log2(self) -> Self {
+                    <$t>::log2(self)
+                }
+
+                fn log10(self) -> Self {
+                    <$t>::log10(self)
+                }
+
+                fn cbrt(self) -> Self {
+                    <$t>::cbrt(self)
+                }
+
+                fn sin(self) -> Self {
+                    <$t>::sin(self)
+                }
+
+                fn cos(self) -> Self {
+                    <$t>::cos(self)
+                }
+
+                fn tan(self) -> Self {
+                    <$t>::tan(self)
+                }
+
+                fn asin(self) -> Self {
+                    <$t>::asin(self)
+                }
+
+                fn acos(self) -> Self {
+                    <$t>::acos(self)
+                }
+
+                fn atan(self) -> Self {
+                    <$t>::atan(self)
+                }
+
+                fn atan2(self, other: Self) -> Self {
+                    <$t>::atan2(self, other)
+                }
+
+                fn sinh(self) -> Self {
+                    <$t>::sinh(self)
+                }
+
+                fn cosh(self) -> Self {
+                    <$t>::cosh(self)
+                }
+
+                fn tanh(self) -> Self {
+                    <$t>::tanh(self)
+                }
+
+                fn asinh(self) -> Self {
+                    <$t>::asinh(self)
+                }
+
+                fn acosh(self) -> Self {
+                    <$t>::acosh(self)
+                }
+
+                fn atanh(self) -> Self {
+                    <$t>::atanh(self)
+                }
+
+                fn recip(self) -> Self {
+                    <$t>::recip(self)
+                }
+
+                fn exp_m1(self) -> Self {
+                    <$t>::exp_m1(self)
+                }
+
+                fn ln_1p(self) -> Self {
+                    <$t>::ln_1p(self)
+                }
+
+                fn e() -> Self {
+                    std::$t::consts::E
+                }
+
+                fn sqrt_2() -> Self {
+                    std::$t::consts::SQRT_2
+                }
+
+                fn pi() -> Self {
+                    std::$t::consts::PI
+                }
+
+                fn one_over_pi() -> Self {
+                    std::$t::consts::FRAC_1_PI
+                }
+
+                fn two_over_pi() -> Self {
+                    std::$t::consts::FRAC_2_PI
+                }
+
+                fn two_over_sqrt_pi() -> Self {
+                    std::$t::consts::FRAC_2_SQRT_PI
+                }
+
+                fn one_over_sqrt_two() -> Self {
+                    std::$t::consts::FRAC_1_SQRT_2
+                }
+
+                fn pi_over_2() -> Self {
+                    std::$t::consts::FRAC_PI_2
+                }
+
+                fn pi_over_3() -> Self {
+                    std::$t::consts::FRAC_PI_3
+                }
+
+                fn pi_over_4() -> Self {
+                    std::$t::consts::FRAC_PI_4
+                }
+
+                fn pi_over_6() -> Self {
+                    std::$t::consts::FRAC_PI_6
+                }
+
+                fn pi_over_8() -> Self {
+                    std::$t::consts::FRAC_PI_8
+                }
+
+                fn ln_2() -> Self {
+                    std::$t::consts::LN_2
+                }
+
+                fn ln_10() -> Self {
+                    std::$t::consts::LN_10
+                }
+
+                fn log2_e() -> Self {
+                    std::$t::consts::LOG2_E
+                }
+
+                fn log10_e() -> Self {
+                    std::$t::consts::LOG10_E
+                }
+
+                fn from_scalar<T: Scalar>(t: T) -> Self {
+                    t.$func()
+                }
+
+                // Adapted from the relative_eq method in the approx crate.
+                // https://docs.rs/approx/0.3.2/src/approx/relative_eq.rs.html#54-83
+                fn approx_eq(self, other: Self, epsilon: Self, max_relative: Self) -> bool {
+                    // The special case in which both are *exactly* equal.
+                    if self == other {
+                        return true;
+                    } else if self.is_infinite() || other.is_infinite() || self.is_nan() || other.is_nan() {
+                        return false;
+                    }
+
+                    let diff = (self - other).abs();
+
+                    if diff <= epsilon {
+                        true
+                    } else {
+                        let abs_self = self.abs();
+                        let abs_other = other.abs();
+
+                        let largest = Self::max(abs_self, abs_other);
+
+                        diff < largest * max_relative
                     }
                 }
             }
@@ -185,197 +431,20 @@ scalar_impls! {
     f64, 0.0, 1.0
 }
 
-impl Real for f64 {
-    fn e() -> Self {
-        std::f64::consts::E
-    }
-
-    fn sqrt_2() -> Self {
-        std::f64::consts::SQRT_2
-    }
-
-    fn pi() -> Self {
-        std::f64::consts::PI
-    }
-
-    fn one_over_pi() -> Self {
-        std::f64::consts::FRAC_1_PI
-    }
-
-    fn two_over_pi() -> Self {
-        std::f64::consts::FRAC_2_PI
-    }
-
-    fn two_over_sqrt_pi() -> Self {
-        std::f64::consts::FRAC_2_SQRT_PI
-    }
-
-    fn one_over_sqrt_two() -> Self {
-        std::f64::consts::FRAC_1_SQRT_2
-    }
-
-    fn pi_over_2() -> Self {
-        std::f64::consts::FRAC_PI_2
-    }
-
-    fn pi_over_3() -> Self {
-        std::f64::consts::FRAC_PI_3
-    }
-
-    fn pi_over_4() -> Self {
-        std::f64::consts::FRAC_PI_4
-    }
-
-    fn pi_over_6() -> Self {
-        std::f64::consts::FRAC_PI_6
-    }
-
-    fn pi_over_8() -> Self {
-        std::f64::consts::FRAC_PI_8
-    }
-
-    fn ln_2() -> Self {
-        std::f64::consts::LN_2
-    }
-
-    fn ln_10() -> Self {
-        std::f64::consts::LN_10
-    }
-
-    fn log2_e() -> Self {
-        std::f64::consts::LOG2_E
-    }
-
-    fn log10_e() -> Self {
-        std::f64::consts::LOG10_E
-    }
-
-    fn from_scalar<T: Scalar>(t: T) -> Self {
-        t.to_f64()
-    }
-
-    // Adapted from the relative_eq method in the approx crate.
-    // https://docs.rs/approx/0.3.2/src/approx/relative_eq.rs.html#54-83
-    fn approx_eq(self, other: Self, epsilon: Self, max_relative: Self) -> bool {
-        // The special case in which both are *exactly* equal.
-        if self == other {
-            return true;
-        } else if self.is_infinite() || other.is_infinite() || self.is_nan() || other.is_nan() {
-            return false;
-        }
-
-        let diff = (self - other).abs();
-
-        if diff <= epsilon {
-            true
-        } else {
-            let abs_self = self.abs();
-            let abs_other = other.abs();
-
-            let largest = Self::max(abs_self, abs_other);
-
-            diff < largest * max_relative
-        }
-    }
-}
-
-impl Real for f32 {
-    fn e() -> Self {
-        std::f32::consts::E
-    }
-
-    fn sqrt_2() -> Self {
-        std::f32::consts::SQRT_2
-    }
-
-    fn pi() -> Self {
-        std::f32::consts::PI
-    }
-
-    fn one_over_pi() -> Self {
-        std::f32::consts::FRAC_1_PI
-    }
-
-    fn two_over_pi() -> Self {
-        std::f32::consts::FRAC_2_PI
-    }
-
-    fn two_over_sqrt_pi() -> Self {
-        std::f32::consts::FRAC_2_SQRT_PI
-    }
-
-    fn one_over_sqrt_two() -> Self {
-        std::f32::consts::FRAC_1_SQRT_2
-    }
-
-    fn pi_over_2() -> Self {
-        std::f32::consts::FRAC_PI_2
-    }
-
-    fn pi_over_3() -> Self {
-        std::f32::consts::FRAC_PI_3
-    }
-
-    fn pi_over_4() -> Self {
-        std::f32::consts::FRAC_PI_4
-    }
-
-    fn pi_over_6() -> Self {
-        std::f32::consts::FRAC_PI_6
-    }
-
-    fn pi_over_8() -> Self {
-        std::f32::consts::FRAC_PI_8
-    }
-
-    fn ln_2() -> Self {
-        std::f32::consts::LN_2
-    }
-
-    fn ln_10() -> Self {
-        std::f32::consts::LN_10
-    }
-
-    fn log2_e() -> Self {
-        std::f32::consts::LOG2_E
-    }
-
-    fn log10_e() -> Self {
-        std::f32::consts::LOG10_E
-    }
-
-    fn from_scalar<T: Scalar>(t: T) -> Self {
-        t.to_f32()
-    }
-
-    // Adapted from the relative_eq method in the approx crate.
-    // https://docs.rs/approx/0.3.2/src/approx/relative_eq.rs.html#54-83
-    fn approx_eq(self, other: Self, epsilon: Self, max_relative: Self) -> bool {
-        // The special case in which both are *exactly* equal.
-        if self == other {
-            return true;
-        } else if self.is_infinite() || other.is_infinite() || self.is_nan() || other.is_nan() {
-            return false;
-        }
-
-        let diff = (self - other).abs();
-
-        if diff <= epsilon {
-            true
-        } else {
-            let abs_self = self.abs();
-            let abs_other = other.abs();
-
-            let largest = Self::max(abs_self, abs_other);
-
-            diff < largest * max_relative
-        }
-    }
+signed_impls! {
+    i8,
+    i16,
+    i32,
+    i64,
+    i128,
+    isize,
+    f32,
+    f64
 }
 
 real_impls! {
-    f64,
-    f32
+    f64, to_f64,
+    f32, to_f32
 }
 
 mod sealed {
@@ -398,12 +467,29 @@ mod sealed {
         + Sub<Output = T>
         + SubAssign<T>
         + PartialEq<T>
+        + ToFloat
     {
+    }
+
+    pub trait ToFloat {
+        fn to_f64(self) -> f64;
+
+        fn to_f32(self) -> f32;
     }
 
     macro_rules! ops_impls {
         ($($t:ty),+) => {
             $(
+                impl self::ToFloat for $t {
+                    fn to_f64(self) -> f64 {
+                        self as f64
+                    }
+
+                    fn to_f32(self) -> f32 {
+                        self as f32
+                    }
+                }
+
                 impl self::Ops<$t> for $t {}
             )*
         }
